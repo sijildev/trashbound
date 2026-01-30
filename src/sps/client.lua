52fd642bfa73e9f35a13ddf0ec7a5937f10f0602
sijildev <sigildevelopmenthelp@gmail.com>
2026-01-30 13:28:29 -0500

    Update client.lua

--- src/sps/client.lua
+++ src/sps/client.lua
@@ -1,9 +1,9 @@
 --// Initializes Client-Sided Modules
 
 -- Roblox Services
-local RepStorage = game:GetService("ReplicatedStorage")
+local repStorage = game:GetService("ReplicatedStorage")
 
-for _, module in pairs(RepStorage:GetDescendants()) do
+for _, module in pairs(repStorage:GetDescendants()) do
 	if module:IsA("ModuleScript") then
 		require(module)
 	end
